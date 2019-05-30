3eb86ee096d7efccd03ae66381dc0fa76229a3dc
GSidi <45037105+GSidi@users.noreply.github.com>
2019-05-30 17:44:57 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -12,13 +12,6 @@ class MyParser:
 	
 	""" A class encapsulating all parsing functionality
 	for a particular grammar. """
-	
-	def create_scanner(self,fp):
-		
-		# create and store the scanner object
-		self.scanner = plex.Scanner(self.LEXICON, fp)
-		# get initial lookahead
-       		self.la, self.text = self.next_token()
 		
 	def __init__(self):
 		letter = plex.Range("Azaz")
@@ -49,14 +42,15 @@ class MyParser:
                                              (id_token, 'id')])
 	
 		self.ST = {}
+		
+	def create_scanner(self,fp):
+		self.scanner = plex.Scanner(self.LEXICON, fp)
+       		self.la, self.text = self.next_token()	
 
 	def next_token(self):
-		""" Returns tuple (next_token,matched-text). """
 		return self.scanner.read()		
 
 	def match(self,token):
-		""" Consumes (matches with current lookahead) an expected token.
-		Raises ParseError if anything else is found. Acquires new lookahead. """ 
 		
 		if self.la==token:
 			self.la,self.text = self.next_token()
@@ -65,19 +59,13 @@ class MyParser:
 	
 	
 	def parse(self,fp):
-		""" Creates scanner for input file object fp and calls the parse logic code. """
-		
-		# create the plex scanner for fp
 		self.create_scanner(fp)
-		
-		# call parsing logic
 		self.stmt_list()
 		 	
-	
 	def stmt_list(self):
        		 if self.la in ("id", "print"):
-          		  self.stmt()
-       	     		  self.stmt_list()
+          		self.stmt()
+       	     		self.stmt_list()
         	 elif self.la == None:
            		  return
         	 else:
@@ -85,54 +73,55 @@ class MyParser:
 	
 	 def stmt(self):
        		 if self.la == 'id':
-		    	varname = self.text
-           		 self.match('id')
-            		 self.match('=')
-            		 self.st[varname] = self.expr()
+		    varname = self.text
+           	    self.match('id')
+            	    self.match('=')
+            	    self.st[varname] = self.expr()
        		 elif self.la == 'print':
-          		  self.match('print')
-           		  print('{:b}'.format(self.expr()))
-        	else:
-            		raise ParseError("{} wasn't an 'id' or 'print' token!".format(self.la))
+       		    self.match('print')
+                    print('{:b}'.format(self.expr()))
+        	 else:
+            	    raise ParseError("{} wasn't an 'id' or 'print' token!".format(self.la))
 
     	def expr(self):
-      		 if self.la in ('(', 'id', 'binary_num'):
-           	 	t = self.term()
-           		while self.la == 'xor':
-               		    self.match('xor')
-                	    t2 = self.term()
-                	    t ^= t2
+      	    if self.la in ('(', 'id', 'binary_num'):
+           	 t = self.term()
+           	 while self.la == 'xor':
+               	    self.match('xor')
+                    t2 = self.term()
+                    t ^= t2
            	 if self.la in ('id', 'print', ')', None):
-               		    return t
-		    	    raise ParseError("{} wasn't an 'id', 'print', ')' or 'None' token!".format(self.la))
-        	 else:
-           		     raise ParseError("{} wasn't an '(', 'id' or 'binary_num' token!".format(self.la))
+               	    return t
+		 raise ParseError("{} wasn't an 'id', 'print', ')' or 'None' token!".format(self.la))
+		
+            else:
+              raise ParseError("{} wasn't an '(', 'id' or 'binary_num' token!".format(self.la))
 
     	def term(self):
         	if self.la in ('(', 'id', 'binary_num'):
-           		 f = self.factor()
+           		f = self.factor()
 		    	while self.la == 'or':
                 		self.match('or')
                		  	f2 = self.factor()
                			f |= f2
-            	if self.la in ('xor', 'id', 'print', ')', None):
-                	return f
+            		if self.la in ('xor', 'id', 'print', ')', None):
+                		return f
             		raise ParseError("{} wasn't an 'xor', 'id', 'print', ')' or 'None' token!".format(self.la))
        	        else:
            		raise ParseError("{} wasn't an '(', 'id' or 'binary_num' token!".format(self.la))
 
    	 def factor(self):
         	if self.la in ('(', 'id', 'binary_num'):
-            		a = self.atom()
+            		 a = self.atom()
             	      while self.la == 'and':
                		 self.match('and')
                 	 a2 = self.atom()
                 	 a &= a2
-		if self.la in ('or', 'xor', 'id', 'print', ')', None):
-                	return a
-            		raise ParseError("{} wasn't an 'or' ,'xor', 'id', 'print', ')' or 'None' token!".format(self.la))
+		      if self.la in ('or', 'xor', 'id', 'print', ')', None):
+                	  return a
+            	      raise ParseError("{} wasn't an 'or' ,'xor', 'id', 'print', ')' or 'None' token!".format(self.la))
         	else:
-           		 raise ParseError("{} wasn't an '(', 'id' or 'b_num' token!".format(self.la))
+           	    raise ParseError("{} wasn't an '(', 'id' or 'b_num' token!".format(self.la))
 
     	def atom(self):
         	if self.la == '(':
@@ -143,15 +132,15 @@ class MyParser:
       	        elif self.la == 'id':
             		varname = self.text
             		self.match('id')
-            	if varname in self.st:
-               		return self.st[varname]
+            		if varname in self.st:
+               			return self.st[varname]
             		raise ParseError("{} was not in in self.ST expected!".format(varname))
        		elif self.la == 'binary_num':
 		    binary_value = int(self.text, 2)
             	    self.match('binary_num')
            	    return binary_value
        		 else:
-           		 raise ParseError("{} wasn't an '(', 'id' or 'binary_num' token!".format(self.la))
+           		raise ParseError("{} wasn't an '(', 'id' or 'binary_num' token!".format(self.la))
 
 parser = MyParser()
 with open("test.txt") as fp:
